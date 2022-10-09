baadccd3b100a8da584bd497d2ade778774a8f47
fako10 <ngadene@gmail.com>
2022-10-09 19:23:32 +0200

    fisrt

---
+++ src/main/java/com/example/ceertifications/CeertificationsApplication.java
@@ -0,0 +1,13 @@
+package com.example.ceertifications;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CeertificationsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CeertificationsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ceertifications/CeertificationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ceertifications;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CeertificationsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
